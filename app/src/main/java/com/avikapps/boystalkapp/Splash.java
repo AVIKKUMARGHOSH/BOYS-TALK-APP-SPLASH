67dceae1a4f4553a551111e312b395ea0119f59d
AVIK KUMAR GHOSH <avikkghosh1998@gmail.com>
2019-03-07 21:36:56 +0530

    First Commit

---
+++ app/src/main/java/com/avikapps/boystalkapp/Splash.java
@@ -0,0 +1,33 @@
+package com.avikapps.boystalkapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent mySuperIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(mySuperIntent);
+
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
